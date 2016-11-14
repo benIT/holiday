eb434ac684c38daa1212520824a1f6c4f6915aa8
ben <benoit.works@gmail.com>
2016-11-14 15:44:40 +0100

    rework unit test

--- tests/FrenchHolidayTest.php
+++ tests/FrenchHolidayTest.php
@@ -8,27 +8,28 @@ class FrenchHolidayTest extends \PHPUnit_Framework_TestCase
 {
     public function testFrenchHolidays()
     {
-        $year            = 2016;
-        $expectedHoliday = [
+        $year             = 2016;
+        $expectedHolidays = [
             new \DateTime(sprintf('%d-%d-%d', $year, 3, 28)),
             new \DateTime(sprintf('%d-%d-%d', $year, 5, 5)),
             new \DateTime(sprintf('%d-%d-%d', $year, 5, 16)),
-            new \DateTime(sprintf('%d-%d-%d', $year, 1, 1)), // 1er janvier
-            new \DateTime(sprintf('%d-%d-%d', $year, 5, 1)), // Fête du travail
-            new \DateTime(sprintf('%d-%d-%d', $year, 5, 8)),  // Victoire des alliés
-            new \DateTime(sprintf('%d-%d-%d', $year, 7, 14)),  // Fête nationale
-            new \DateTime(sprintf('%d-%d-%d', $year, 8, 15)),  // Assomption
-            new \DateTime(sprintf('%d-%d-%d', $year, 11, 01)),  // Toussaint
-            new \DateTime(sprintf('%d-%d-%d', $year, 11, 11)),  // Armistice
-            new \DateTime(sprintf('%d-%d-%d', $year, 12, 25)),  // Noel
+            new \DateTime(sprintf('%d-%d-%d', $year, 1, 1)),
+            new \DateTime(sprintf('%d-%d-%d', $year, 5, 1)),
+            new \DateTime(sprintf('%d-%d-%d', $year, 5, 8)),
+            new \DateTime(sprintf('%d-%d-%d', $year, 7, 14)),
+            new \DateTime(sprintf('%d-%d-%d', $year, 8, 15)),
+            new \DateTime(sprintf('%d-%d-%d', $year, 11, 01)),
+            new \DateTime(sprintf('%d-%d-%d', $year, 11, 11)),
+            new \DateTime(sprintf('%d-%d-%d', $year, 12, 25)),
         ];
-        $holidays       = FrenchHoliday::getHolidays($year);
-        $holidaysString = array_map(function ($holiday) {
+
+        $processedHolidays       = FrenchHoliday::getHolidays($year);
+        $processedHolidaysString = array_map(function ($holiday) {
             return $holiday->format('y-m-d') ;
-        }, $holidays);
+        }, $processedHolidays);
         
-        foreach ($holidays as $holiday) {
-            $this->assertTrue(in_array($holiday->format('y-m-d'), $holidaysString), $holiday->format('y-m-d').'is not expected to be holiday.');
+        foreach ($expectedHolidays as $expectedHoliday) {
+            $this->assertTrue(in_array($expectedHoliday->format('y-m-d'), $processedHolidaysString), $expectedHoliday->format('y-m-d').' is expected to be holiday.');
         }
     }
 }
