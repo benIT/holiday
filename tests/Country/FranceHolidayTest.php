e9a722343edcd139078b10960043f373eba1208f
ben <benoit.works@gmail.com>
2016-11-23 11:30:51 +0100

    rework unit test with setUpBeforeClass method

--- tests/Country/FranceHolidayTest.php
+++ tests/Country/FranceHolidayTest.php
@@ -4,40 +4,57 @@ namespace Holiday\Tests\Country;
 use Holiday\Model\AbstractHoliday;
 use Holiday\Country\FranceHoliday;
 
-class FrancehHolidayTest extends \PHPUnit_Framework_TestCase
+class FranceHolidayTest extends \PHPUnit_Framework_TestCase
 {
+    private static $expectedHolidays ;
+    
+    private static $year;
+    
+    private static $processedHolidays;
+    
+    private static $processedHolidaysString;
+    
+    public static function setUpBeforeClass()
+    {
+        self::$year                     =2016 ;
+        self::$expectedHolidays         = [
+            new \DateTime(sprintf('%d-%d-%d',    self::$year, 3, 28)),
+            new \DateTime(sprintf('%d-%d-%d',    self::$year, 5, 5)),
+            new \DateTime(sprintf('%d-%d-%d',    self::$year, 5, 16)),
+            new \DateTime(sprintf('%d-%d-%d',    self::$year, 1, 1)),
+            new \DateTime(sprintf('%d-%d-%d',    self::$year, 5, 1)),
+            new \DateTime(sprintf('%d-%d-%d',    self::$year, 5, 8)),
+            new \DateTime(sprintf('%d-%d-%d',    self::$year, 7, 14)),
+            new \DateTime(sprintf('%d-%d-%d',    self::$year, 8, 15)),
+            new \DateTime(sprintf('%d-%d-%d',    self::$year, 11, 01)),
+            new \DateTime(sprintf('%d-%d-%d',    self::$year, 11, 11)),
+            new \DateTime(sprintf('%d-%d-%d',    self::$year, 12, 25)),
+        ];
+        
+        self::$processedHolidays               = FranceHoliday::getHolidays(self::$year);
+        self::$processedHolidaysString         = array_map(function ($holiday) {
+            return $holiday->format('y-m-d') ;
+        }, self::$processedHolidays);
+    }
+
+    
     public function testFranceHolidays()
     {
-        $year             = 2016;
-        $expectedHolidays = [
-            new \DateTime(sprintf('%d-%d-%d', $year, 3, 28)),
-            new \DateTime(sprintf('%d-%d-%d', $year, 5, 5)),
-            new \DateTime(sprintf('%d-%d-%d', $year, 5, 16)),
-            new \DateTime(sprintf('%d-%d-%d', $year, 1, 1)),
-            new \DateTime(sprintf('%d-%d-%d', $year, 5, 1)),
-            new \DateTime(sprintf('%d-%d-%d', $year, 5, 8)),
-            new \DateTime(sprintf('%d-%d-%d', $year, 7, 14)),
-            new \DateTime(sprintf('%d-%d-%d', $year, 8, 15)),
-            new \DateTime(sprintf('%d-%d-%d', $year, 11, 01)),
-            new \DateTime(sprintf('%d-%d-%d', $year, 11, 11)),
-            new \DateTime(sprintf('%d-%d-%d', $year, 12, 25)),
-        ];
-
-        $processedHolidays       = FranceHoliday::getHolidays($year);
-        $processedHolidaysString = array_map(function ($holiday) {
-            return $holiday->format('y-m-d') ;
-        }, $processedHolidays);
-        
-        foreach ($expectedHolidays as $expectedHoliday) {
-            $this->assertTrue(in_array($expectedHoliday->format('y-m-d'), $processedHolidaysString), $expectedHoliday->format('y-m-d').' is expected to be holiday.');
+        foreach (self::$expectedHolidays as $expectedHoliday) {
+            $this->assertTrue(in_array($expectedHoliday->format('y-m-d'), self::$processedHolidaysString), $expectedHoliday->format('y-m-d').' is expected to be holiday.');
         }
     }
     
     
-     public function testClosedDaysFrance() {
-     	$this->assertTrue( FranceHoliday::isClosedDay(new \DateTime(sprintf('%d-%d-%d', 2016, 3, 28))));
-     	$this->assertTrue( FranceHoliday::isClosedDay(new \DateTime(sprintf('%d-%d-%d', 2016, 12, 25)))); 
-     	
-     }
-    
+    public function testClosedDaysFrance()
+    {
+        $processedHolidays               = FranceHoliday::getHolidays(self::$year);
+        $processedHolidaysString         = array_map(function ($holiday) {
+            return $holiday->format('y-m-d') ;
+        }, $processedHolidays);
+        
+        foreach ($processedHolidaysString as $processedHoliday) {
+            $this->assertTrue(FranceHoliday::isClosedDay(new \DateTime($processedHoliday)));
+        }
+    }
 }
--- tests/Country/UsaHolidayTest.php
+++ tests/Country/UsaHolidayTest.php
@@ -6,29 +6,55 @@ use Holiday\Country\UsaHoliday;
 
 class UsaHolidayTest extends \PHPUnit_Framework_TestCase
 {
-    public function testUsHolidays()
+    private static $expectedHolidays ;
+    
+    private static $year;
+    
+    private static $processedHolidays;
+    
+    private static $processedHolidaysString;
+    
+    public static function setUpBeforeClass()
     {
-        $year             = 2016;
-        $expectedHolidays = [
-            new \DateTime(sprintf('%d-%d-%d', $year, 1, 1)),
-            new \DateTime(sprintf('%d-%d-%d', $year, 1, 18)),
-            new \DateTime(sprintf('%d-%d-%d', $year, 3, 27)),
-            new \DateTime(sprintf('%d-%d-%d', $year, 5, 30)),
-            new \DateTime(sprintf('%d-%d-%d', $year, 7, 4)),
-            new \DateTime(sprintf('%d-%d-%d', $year, 9, 5)),
-            new \DateTime(sprintf('%d-%d-%d', $year, 11, 11)),
-            new \DateTime(sprintf('%d-%d-%d', $year, 11, 24)),
-            new \DateTime(sprintf('%d-%d-%d', $year, 12, 24)),
-            new \DateTime(sprintf('%d-%d-%d', $year, 12, 26)),
-            new \DateTime(sprintf('%d-%d-%d', $year, 12, 31)),
+        self::$year                     =2016 ;
+        self::$expectedHolidays         = [
+            new \DateTime(sprintf('%d-%d-%d', self::$year, 1, 1)),
+            new \DateTime(sprintf('%d-%d-%d', self::$year, 1, 18)),
+            new \DateTime(sprintf('%d-%d-%d', self::$year, 3, 27)),
+            new \DateTime(sprintf('%d-%d-%d', self::$year, 5, 30)),
+            new \DateTime(sprintf('%d-%d-%d', self::$year, 7, 4)),
+            new \DateTime(sprintf('%d-%d-%d', self::$year, 9, 5)),
+            new \DateTime(sprintf('%d-%d-%d', self::$year, 11, 11)),
+            new \DateTime(sprintf('%d-%d-%d', self::$year, 11, 24)),
+            new \DateTime(sprintf('%d-%d-%d', self::$year, 12, 24)),
+            new \DateTime(sprintf('%d-%d-%d', self::$year, 12, 26)),
+            new \DateTime(sprintf('%d-%d-%d', self::$year, 12, 31)),
         ];
-        $processedHolidays       = UsaHoliday::getHolidays($year);
-        $processedHolidaysString = array_map(function ($holiday) {
+        
+        self::$processedHolidays       = UsaHoliday::getHolidays(self::$year);
+        self::$processedHolidaysString         = array_map(function ($holiday) {
+            return $holiday->format('y-m-d') ;
+        }, self::$processedHolidays);
+    }
+
+    
+    public function testUsaHolidays()
+    {
+        foreach (self::$expectedHolidays as $expectedHoliday) {
+            $this->assertTrue(in_array($expectedHoliday->format('y-m-d'), self::$processedHolidaysString), $expectedHoliday->format('y-m-d').' is expected to be holiday.');
+        }
+    }
+    
+    
+    public function testClosedDaysUsa()
+    {
+        $processedHolidays               = UsaHoliday::getHolidays(self::$year);
+        $processedHolidaysString         = array_map(function ($holiday) {
             return $holiday->format('y-m-d') ;
         }, $processedHolidays);
         
-        foreach ($expectedHolidays as $expectedHoliday) {
-            $this->assertTrue(in_array($expectedHoliday->format('y-m-d'), $processedHolidaysString), $expectedHoliday->format('y-m-d').' is expected to be holiday.');
+        foreach ($processedHolidaysString as $processedHoliday) {
+            $this->assertTrue(UsaHoliday::isClosedDay(new \DateTime($processedHoliday)));
         }
     }
 }
