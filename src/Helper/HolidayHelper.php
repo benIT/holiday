40983fc3c4bcc9599ed0b99ba9f21afe97ef36f1
ben <benoit.works@gmail.com>
2016-11-23 15:35:40 +0100

    add isCallable test

--- src/Helper/HolidayHelper.php
+++ src/Helper/HolidayHelper.php
@@ -7,7 +7,7 @@ class HolidayHelper
  	{
  		$classNamePattern = 'Holiday\Country\%sHoliday';
  		if (class_exists(sprintf($classNamePattern, $country))) {
- 			if (method_exists(sprintf($classNamePattern, $country), $method)) {
+ 			if (method_exists(sprintf($classNamePattern, $country), $method) && is_callable(sprintf($classNamePattern, $country), $method)) {
             	return call_user_func_array(sprintf($classNamePattern.'::%s', $country, $method), $params);
  			} else {
  				 throw new \Exception(sprintf('The required method "%s" does not exist for %s', $method, sprintf($classNamePattern, $country))); 
